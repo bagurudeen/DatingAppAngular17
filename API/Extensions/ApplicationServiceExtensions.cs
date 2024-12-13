22794ff71bbbb49c934af74718081d9c79e40817
bagur <mohamedbagurudeen@gmail.com>
2024-12-13 20:57:03 +0530

    All code commit API and UI

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddControllers();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            //opt.UseSqlServer(config.GetConnectionString("DefaultConnectionString"));
+            opt.UseSqlite(config.GetConnectionString("DefaultConnectionString"));
+        });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ILikesRepository, LikesRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<IPhotoRepository, PhotoRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IPhotoService, PhotoService>();
+        services.AddScoped<LogUserActivity>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+        services.AddSignalR();
+        services.AddSingleton<PresenceTracker>();
+
+        return services;
+    }
+}
